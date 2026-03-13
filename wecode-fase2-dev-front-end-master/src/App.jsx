23591cc397c2c631c7c2565f331a113eb6a1b015
guDutra <gudutra05@outlook.com>
2026-03-12 22:07:32 -0300

    .env

--- wecode-fase2-dev-front-end-master/src/App.jsx
+++ wecode-fase2-dev-front-end-master/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.scss';
 import { Main } from './pages/Main';
-import MainOne from './pages/MainOne';
 function App() {
   return (
     <div className="App">
       <Main/>
-      {/*<MainOne/>*/}
     </div>
   );
 }
